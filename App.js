312c6f0320637a233fbc0b5d2421174e0a365831
studentAH <afn.haq.stuff@gmail.com>
2025-07-01 19:29:56 -0400

    sample App.js to see Generic Album Page

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { View, ActivityIndicator, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -67,3 +67,20 @@ const styles = StyleSheet.create({
     marginBottom: 72, // Reserve space for the footer height
   },
 });
+
+*/
+
+//App.js test file, use to view Generic Album Page Component
+
+import { View } from 'react-native';
+
+import GenericAlbumPage from './src/screens/GenericAlbumTest.js'
+
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 , backgroundColor: 'white'}}>
+        <GenericAlbumPage/>
+    </View>
+  );
+}
