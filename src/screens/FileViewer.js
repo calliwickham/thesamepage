f587fe90b1978b87d9a009ec97747e9ae57b1214
studentAH <afn.haq.stuff@gmail.com>
2025-07-05 21:11:42 -0400

    added functioning back arrow

--- src/screens/FileViewer.js
+++ src/screens/FileViewer.js
@@ -1,22 +1,26 @@
 //For now this screen is very ugly, just a placeholder.
 
-import { SafeAreaView, ScrollView, StyleSheet, View, Text, Image } from 'react-native';
-import { useRoute } from "@react-navigation/native";
+import { SafeAreaView, ScrollView, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { useRoute, useNavigation } from "@react-navigation/native";
 import NavArrow from '../newcomps/NavArrow';
 
-export default function FileViewer(){
-    
+export default function FileViewer() {
+
+    const navigation = useNavigation();
     const route = useRoute();
     const { file } = route.params;
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{marginTop: 4}}>
-                <NavArrow/>
-            </View>
-            <Text style={{color: 'blue', fontWeight: 'bold', margin: 10}}> In Development: Back arrow doesn't work yet </Text>
+            <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                activeOpacity={0.4}
+                style={{marginTop: 4}}
+            >
+                <NavArrow />
+            </TouchableOpacity>
             <Text style={styles.header}> {file.title} </Text>
-            <Text style={{color: 'red', fontWeight: 'bold', margin: 10}}> This page isn't ready yet! </Text>
+            <Text style={{ color: 'red', fontWeight: 'bold', margin: 10 }}> Still in development: This page isn't complete! </Text>
             <ScrollView contentContainerStyle={styles.scrollContainer}>
                 <Text style={styles.text}> {file.text} </Text>
             </ScrollView>
