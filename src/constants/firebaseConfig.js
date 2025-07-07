e3ea9b36db241d8938a670dd6d16266c92c44af5
studentAH <afn.haq.stuff@gmail.com>
2025-07-06 22:48:10 -0400

    added firebase credentials

---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+export default firebaseConfig = {
+  apiKey: "AIzaSyBSj_Vq4dsrLGSyWrB5C1ScrzvMkV8-bVA",
+  authDomain: "thesamepage-43fb0.firebaseapp.com",
+  projectId: "thesamepage-43fb0",
+  storageBucket: "thesamepage-43fb0.firebasestorage.app",
+  messagingSenderId: "34043205967",
+  appId: "1:34043205967:web:23bbb2774560f3cc228d3a"
+};
+
+// Initialize Firebase
+//const firebaseApp = initializeApp(firebaseConfig);
